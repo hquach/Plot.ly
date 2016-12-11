ff05c4a4016de7caba60d96b94ec1d51212a8e32
Hoa Kevin Quach <hquach@users.noreply.github.com>
2016-12-11 14:59:08 -0800

    create Midwest.R
    create Midwest.R

---
+++ Midwest.R
@@ -0,0 +1,14 @@
+#URL: https://plot.ly/~hoaquach/7
+
+# Library
+library(plotly)
+
+# A basic boxplot
+library(plotly)
+p <- plot_ly(midwest, x = ~percollege, color = ~state, type = "box")
+p
+
+plotly_POST(p, 
+            filename = "midwest-boxplot", 
+            fileopt="overwrite", 
+            sharing = "public")
